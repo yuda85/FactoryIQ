d9589244020b81aefb4c2ab18394040dd22e35a3
yehuda buzaglo <yehudabu@rafael.co.il>
2024-12-16 14:31:30 +0200

    added pie chart

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideCharts(withDefaultRegisterables()),
+  ],
 };
--- src/app/machines/machines-dashboard/machines-dashboard.component.ts
+++ src/app/machines/machines-dashboard/machines-dashboard.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MachinesService } from '../../services/machines.service';
 import { Subscription } from 'rxjs';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+// import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js'/;
+// import { BaseChartDirective } from 'ng2-charts';
+import { MatButton } from '@angular/material/button';
+import { MatDivider } from '@angular/material/divider';
+// import { ChartHostComponent } from '../chart-host/chart-host.component';
 
 @Component({
   selector: 'app-machines-dashboard',
-  imports: [],
+  imports: [BaseChartDirective, MatButton, MatDivider],
   templateUrl: './machines-dashboard.component.html',
   styleUrl: './machines-dashboard.component.scss',
 })
@@ -23,5 +30,154 @@ export class MachinesDashboardComponent {
 
   ngOnDestroy() {
     this.sub.unsubscribe();
+  }
+
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  // Pie
+  public pieChartOptions: any = {
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top',
+      },
+      datalabels: {
+        formatter: (value, ctx) => {
+          if (ctx.chart.data.labels) {
+            return ctx.chart.data.labels[ctx.dataIndex];
+          }
+          return '';
+        },
+      },
+    },
+  };
+  public pieChartData: ChartData<'pie', number[], string | string[]> = {
+    labels: ['', '', 'Mail Sales'],
+    datasets: [
+      {
+        data: [300, 500, 100],
+      },
+    ],
+  };
+  public pieChartType: ChartType = 'pie';
+
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event: ChartEvent;
+    active: object[];
+  }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event: ChartEvent;
+    active: object[];
+  }): void {
+    console.log(event, active);
+  }
+
+  changeLabels(): void {
+    const words = [
+      'hen',
+      'variable',
+      'embryo',
+      'instal',
+      'pleasant',
+      'physical',
+      'bomber',
+      'army',
+      'add',
+      'film',
+      'conductor',
+      'comfortable',
+      'flourish',
+      'establish',
+      'circumstance',
+      'chimney',
+      'crack',
+      'hall',
+      'energy',
+      'treat',
+      'window',
+      'shareholder',
+      'division',
+      'disk',
+      'temptation',
+      'chord',
+      'left',
+      'hospital',
+      'beef',
+      'patrol',
+      'satisfied',
+      'academy',
+      'acceptance',
+      'ivory',
+      'aquarium',
+      'building',
+      'store',
+      'replace',
+      'language',
+      'redeem',
+      'honest',
+      'intention',
+      'silk',
+      'opera',
+      'sleep',
+      'innocent',
+      'ignore',
+      'suite',
+      'applaud',
+      'funny',
+    ];
+    const randomWord = () => words[Math.trunc(Math.random() * words.length)];
+    this.pieChartData.labels = new Array(3).map(() => randomWord());
+
+    this.chart?.update();
+  }
+
+  addSlice(): void {
+    if (this.pieChartData.labels) {
+      this.pieChartData.labels.push(['Line 1', 'Line 2', 'Line 3']);
+    }
+
+    this.pieChartData.datasets[0].data.push(400);
+
+    this.chart?.update();
+  }
+
+  removeSlice(): void {
+    if (this.pieChartData.labels) {
+      this.pieChartData.labels.pop();
+    }
+
+    this.pieChartData.datasets[0].data.pop();
+
+    this.chart?.update();
+  }
+
+  changeLegendPosition(): void {
+    if (this.pieChartOptions?.plugins?.legend) {
+      this.pieChartOptions.plugins.legend.position =
+        this.pieChartOptions.plugins.legend.position === 'left'
+          ? 'top'
+          : 'left';
+    }
+
+    this.chart?.render();
+  }
+
+  toggleLegend(): void {
+    if (this.pieChartOptions?.plugins?.legend) {
+      this.pieChartOptions.plugins.legend.display =
+        !this.pieChartOptions.plugins.legend.display;
+    }
+
+    this.chart?.render();
   }
 }
